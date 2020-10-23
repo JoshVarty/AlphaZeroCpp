5d406bb7bf7e89e25e6419d545bd9ea9e5dcda59
Josh Varty <joshvarty@gmail.com>
2020-10-22 20:29:33 -0400

    Add two tests for model

--- test/model_tests.cpp
+++ test/model_tests.cpp
@@ -5,10 +5,19 @@
 TEST(ModelTests, EnsureWeCanCreateModel) {
     Connect2Model model(4, 4, torch::kCPU);
 
-    auto options = torch::TensorOptions().dtype(torch::kFloat32);
-    auto x = torch::zeros({1, 4}, options);
-    auto sz = x.sizes();
-    
-    auto result = model.forward(x);
+    ASSERT_EQ(model._boardSize, 4);
+    ASSERT_EQ(model._actionSize, 4);
+    ASSERT_EQ(model._device, torch::kCPU);
+}
 
+TEST(ModelTests, EnsureWeCanPassZeroThroughNetwork) {
+    Connect2Model model(4, 4, torch::kCPU);
+
+    auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCPU);
+    auto input = torch::zeros({1, 4}, options);
+    auto output = model.forward(input);
+
+    ASSERT_EQ(input.size(0), output.action.size(0));
+    ASSERT_EQ(input.size(1), output.action.size(1));
+    ASSERT_EQ(output.value.size(0), 1);
 }
